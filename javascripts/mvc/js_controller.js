ea6f1fa88fd1d62df331d4ed169bbb5fd9783901
topper <topper@toppingdesign.com>
2008-07-15 22:31:53 -0400

    setting up tests

---
+++ javascripts/mvc/js_controller.js
@@ -0,0 +1,341 @@
+if (!("MBX" in window)) {
+    /** @namespace */
+    MBX = {};
+}
+
+/**
+    Create and extend controllers
+    @namespace
+*/
+MBX.JsController = (function () {
+    /**
+        public methods of JsController
+        @memberof MBX.JsController
+        @namespace
+    */
+    var publicObj = {};
+    
+    /**
+        used to cache instances of controllers and stop name collisions
+        @memberof MBX.JsController
+        @private
+    */
+    var controllerCache = {};
+    
+    
+    /** @private */
+    var jsElementClass = '.js_updateable';
+    
+    /**
+        for creating a controller
+        @class prototype of all controllers
+    */
+    JsController = function (name, opts) {
+        /**
+            @default {}
+        */
+        opts = opts || {};
+        if (!name) {
+            throw new Error("A name must be specified");
+        }
+        if (controllerCache[name]) {
+            throw new Error("A controller by that name exists");
+        }
+        
+        this.controllerName = name;
+        Object.extend(this, opts);
+        if (opts.model) {
+            this._subscribeToEvents();
+            this.modelClass = MBX.JsTemplate.cssFromModel(this.model);
+        }
+        controllerCache[name] = this;
+        
+        MBX.EventHandler.fireCustom(document.body, publicObj.Event.newController, {
+            object: this
+        });
+    };
+    
+    JsController.prototype = 
+        /** @lends JsController */
+        {
+        /**
+            @private
+            @requires MBX.JsModel
+            @requires this.model
+            @requires MBX.JsTemplate
+        */
+        _onInstanceChange: function (evt) {
+            this.setDefaultViewOptions();
+            if (this.onInstanceChange) {
+                this.onInstanceChange(evt.object, evt.key);
+            }
+
+            if ((!this.renderNothing) && this.templateToRender) {                
+                this.renderChangeUpdates(evt.object, evt.key);
+            }
+        },
+        
+        /**
+            @private
+            @requires MBX.JsModel
+            @requires this.model
+            @requires MBX.JsTemplate
+        */
+        _onInstanceCreate: function (evt) {
+            this.setDefaultViewOptions();
+            
+            if (this.onInstanceCreate) {
+                this.onInstanceCreate(evt.object);
+            }           
+            
+            if ((!this.renderNothing) && this.templateToRender) {
+                        
+                this.renderCreateUpdates(evt.object);
+            }
+        },
+        
+        /**
+            @private
+            @requires MBX.JsModel
+            @requires this.model
+            @requires MBX.JsTemplate
+        */
+        _onInstanceDestroy: function (evt) {
+            this.setDefaultViewOptions();
+            if (this.onInstanceDestroy) {
+                this.onInstanceDestroy(evt.object);
+            }
+            if (!this.renderNothing) {                
+                this.renderDestroy(evt.object);
+            }
+        },
+        
+        /**
+            You can overwrite this method if you want to handle default changeUpdates differently
+            @param {JsModel#instance} instance the instance that changed
+            @requires MBX.JsModel
+            @requires this.model
+            @requires MBX.JsTemplate
+        */
+        renderChangeUpdates: function (instance) {
+            var elsToUpdate = this.elementsToUpdate;
+            if (!elsToUpdate) {
+                var findString = this._instanceFindString(instance);
+                elsToUpdate = $$(findString);
+            }
+
+            if (elsToUpdate) {
+                elsToUpdate.each(function (el) { 
+                    el.update(MBX.JsTemplate.render(this.templateToRender, instance));
+                }.bind(this));
+            }
+        },
+        
+       // Todo: refactor this create method to better handle the way you would *really* want a collectio nto work
+       //             - existing element stuff is ugly
+       /**
+            Overwrite this method to handle model create events differently
+            @param {JsModel#instance} instance the instance that was created
+            @requires MBX.JsModel
+            @requires this.model
+            @requires MBX.JsTemplate
+       */
+        renderCreateUpdates: function (instance) {
+            var collsToUpdate = this.collectionsToUpdate;
+            if (!collsToUpdate) {
+                var findString = [jsElementClass, '.' , this.modelClass, '.collection'].join('');
+                collsToUpdate = $$(findString);
+            }
+            if (collsToUpdate) {
+                collsToUpdate.each(function (collection) {
+                    var elClass = [jsElementClass, ' ', this.modelClass, ' ', MBX.JsTemplate.cssFromInstance(instance)].join('').gsub(/\./, "");
+                    var elToInsert = new Element('div', { className: elClass });
+                    elToInsert.update(MBX.JsTemplate.render(this.templateToRender, instance));
+                    var collectionId = collection.identify();
+                    
+                    var existingElements = $$(["#", collectionId, " .", this.modelClass, '.', MBX.JsTemplate.cssFromInstance(instance)].join(''));
+                    if (existingElements.length > 0) {
+                        existingElements.each(function (el) {
+                            el.replace(elToInsert);
+                        });
+                    } else {
+                        collection.insert({ bottom: elToInsert });
+                    }
+                }.bind(this));
+            }
+        },
+        
+        /**
+             Overwrite this method to handle model destroy events differently
+             @param {JsModel#instance} instance the instance that was destroyed
+             @requires MBX.JsModel
+             @requires this.model
+             @requires MBX.JsTemplate
+        */
+        renderDestroy: function (instance) {
+            var elsToDestroy = this.elementsToDestroy;
+            if (!elsToDestroy) {
+                var findString = this._instanceFindString(instance);
+                elsToDestroy = $$(findString);
+            }
+            if (elsToDestroy) {
+                elsToDestroy.each(function(el) {
+                    el.remove();
+                });
+            }
+        },
+        
+        /**
+             setDefaultViewOptions is usually used internally to setup each model event as a new "request"
+             you can overwrite this if you want to handle things differently
+             @requires MBX.JsModel
+             @requires this.model
+             @requires MBX.JsTemplate
+             @example
+               MyController = MBX.JsController.create('MyController', {
+                   setDefaultViewOptions: function () {
+                          this.renderNothing = true;
+                    }
+               });
+               // this will default MyController to never render anything
+             
+               
+        */
+        setDefaultViewOptions: function () {
+            this.templateToRender = this._defaultTemplateOrNull();
+            this.renderNothing = false;
+            this.elementsToUpdate = null;
+            this.collectionsToUpdate = null;
+            this.elementsToDestroy = null;
+        },
+        
+        /**
+            @private
+            @requires MBX.JsModel
+        */
+        _instanceFindString: function (instance) {
+            return [jsElementClass, '.', this.modelClass, '.', MBX.JsTemplate.cssFromInstance(instance)].join('');
+        },
+        
+        /**
+            @private
+            @requires MBX.JsModel
+            @requires this.model
+            @requires MBX.JsTemplate
+        */
+        _defaultTemplateOrNull: function () {
+            if (MBX.JsTemplate.find(this.modelClass)) {
+                return this.modelClass;
+            }
+        },
+        
+        /**
+            @private
+            @requires MBX.JsModel
+            @requires this.model
+            
+        */
+        _subscribeToEvents: function () {
+            var changeEvent = this.model.Event.changeInstance;
+            var newEvent = this.model.Event.newInstance;
+            var destroyEvent = this.model.Event.destroyInstance;
+
+            this.eventSubscriptions = [];
+            this.eventSubscriptions.push(MBX.EventHandler.subscribe(MBX.cssNamespace, changeEvent, this._onInstanceChange.bind(this)));
+            this.eventSubscriptions.push(MBX.EventHandler.subscribe(MBX.cssNamespace, newEvent, this._onInstanceCreate.bind(this)));
+            this.eventSubscriptions.push(MBX.EventHandler.subscribe(MBX.cssNamespace, destroyEvent, this._onInstanceDestroy.bind(this)));
+        },
+        
+        /**
+            @private
+            @requires MBX.JsModel
+            @requires this.model
+        */
+        _unsubscribeToEvents: function () {
+            if (this.eventSubscriptions && this.eventSubscriptions[0]) {                
+                this.eventSubscriptions.each(function (subscription) {
+                    MBX.EventHandler.unsubscribe(subscription);
+                });
+            }
+        }
+    };
+    
+    /**
+        This is mostly used internally and is fired on document.body everytime a controller is created
+        @memberOf MBX.JsController
+    */
+    publicObj.Event = {
+        newController: "new_controller"
+    };
+    
+    /**
+        call extend() to add methods and/or attributes to ALL controllers
+        @param {Object} methsAndAttrs
+        @name MBX.JsController.extend
+        @function
+    */
+    publicObj.extend = function (methsAndAttrs) {
+        methsAndAttrs = methsAndAttrs || {};
+        Object.extend(JsController.prototype, methsAndAttrs);
+    };
+    
+    /**
+        Controllers allow some decently powerful hooks. You can specify a model, and an
+        onAfterCreate, onInstanceChange, onInstanceDestroy, onInstanceCreate
+          
+        @name MBX.JsController.create
+        @param {String} name the name of the controller
+        @param {Object} opts used to extend the controller methods at instantiation
+        @see JsController
+        @function
+        @example
+          MBX.DesktopUploadController = MBX.JsController.create("DesktopUpload", {
+              ANewMethod: function (something) {
+                  return something;
+              }
+          })
+          MBX.DesktopUpload.ANewMethod("boo") == "boo";
+        @example
+          MBX.DesktopUploadController = MBX.JsController.create("DesktopUpload", {
+              model: MBX.DesktopUpload,
+              onInstanceCreate: function (instance) {
+                  alert(instance.get('greeting'));
+              }              
+          });
+          MBX.DesktopUpload.create({ greeting: 'hi' });  // will alert('hi');
+        @example
+          MBX.DesktopUploadController = MBX.JsController.create("DesktopUpload", {
+              model: MBX.DesktopUpload,
+              onInstanceChange: function (instance) {
+                  alert(instance.get('greeting'));
+              }              
+          });
+          var instance = MBX.DesktopUpload.create();
+          instance.set('greeting', 'hi');  // will alert('hi')
+    */
+    publicObj.create = function (name, opts) {
+        if (controllerCache[name]) {
+            throw new Error("A controller with the name of " + name + " is already in use");
+        }
+        var controller = new JsController(name, opts);
+        if (typeof controller.afterCreate == "function") {
+            controller.afterCreate();
+        }
+        return controller;
+    };
+    
+    /**
+        Destroy a controller and unsubscribe its event listeners
+        @param {String} name the name of the controller
+        @name MBX.JsController.destroyController
+        @function
+    */
+    publicObj.destroyController = function (name) {
+        if (controllerCache[name]) {
+            controllerCache[name]._unsubscribeToEvents();
+            delete controllerCache[name];
+        }
+    };
+    
+    return publicObj;
+})();
---
+++ javascripts/mvc/js_model.js
@@ -0,0 +1,382 @@
+/**
+    @fileOverview 
+      It all starts with the Model. Just like in Rails, the model should really only deal with data and methods surrounding data. It shouldn't interact with the UI or (and this is up for debate) the server. Things like pagination, etc should be handled in the controller.
+    @example
+      MBX.MyModel = MBX.JsModel.create("MyModel");
+    @author  <a href="mailto:topper@motionbox.com">Topper Bowers</a>
+    @version 0.1  
+*/
+
+if (!("MBX" in window)) {
+    /** @namespace
+        @ignore
+    */
+    MBX = {};
+}
+
+/** 
+    use this as a more convienient (sometimes) method instead of .prototype.blah.prototype chaining.  It tends
+    to be a *real* javascript way of sub-classing
+    
+    @parm {Object} o the original object
+    @returns a new object with the original object as a prototype
+*/
+MBX.Constructor = function (o) {
+    function F() {}
+    F.prototype = o;
+    return new F();
+};
+
+/**
+    Use this to create instances of models and extend all models (and instances of all models)
+    @namespace
+*/
+MBX.JsModel = (function () {
+    /**
+        @memberof MBX.JsModel
+        @namespace
+    */
+    var publicObj = {};
+    var currentGUID = 0;
+    
+    /** used internally to prevent name collision 
+        @private
+    */
+    var modelCache = {};
+    
+    /**
+        Instances of a Model
+        @name JsModel#instance
+        @class A single instance of a Model
+        @see MBX.Constructor
+    */
+    var oneJsModelInstance = 
+        /** @lends JsModel#instance */
+        {
+        /**
+            Use this to set attributes of an instance (rather than set them directly).
+            It will automatically create events that will be relevant to controllers
+            @param {String} key the key of the attribute
+            @param value the value to be assigned to the attribute
+            @example
+              modelInstance.set('myAttr', 'foo');
+              modelInstance.get('myAttr', 'foo');
+        */
+        set: function (key, value) {
+            var changed = false;
+            if (this.attributes[key] != value) {
+                changed = true;
+            }
+            this.attributes[key] = value;
+            if (changed) {
+                MBX.EventHandler.fireCustom(document.body, this.parentClass.Event.changeInstance, {
+                    object: this,
+                    key: key
+                });
+            }
+        },
+        /**
+            Use this to retreive attributes on an object (rather than accessing them directly);
+            @param {String} key
+            @example
+              modelInstance.set('myAttr', 'foo');
+              modelInstance.get('myAttr', 'foo');
+        */
+        get: function (key) {
+            return this.attributes[key];
+        },
+        
+        /**
+            Take an Object literal and update all attributes on this instance
+            @param {Object} obj the attributes to update as key, value
+        */
+        updateAttributes: function (obj) {
+            obj = obj || {};
+            for (k in obj) {
+                if (obj.hasOwnProperty(k)) {
+                    this.set(k, obj[k]);
+                }
+            }
+        },
+        
+        /**
+            You should always use this to refer to instances.
+            Model.find uses this to grab objects from the instances 
+            @returns returns the primaryKey of the instance
+            @see JsModel.find
+        */
+        primaryKey: function () {
+            if (this.parentClass.primaryKey) {
+                return this.get(this.parentClass.primaryKey);
+            } else {
+                return this.GUID;
+            }
+        },
+        
+        /**
+            destroy this instance - works just like rails #destroy will fire off the destroy event as well
+            controllers will receive this event by default
+        */
+        destroy: function () {
+            delete this.parentClass.instanceCache[this.primaryKey()];
+            MBX.EventHandler.fireCustom(document.body, this.parentClass.Event.destroyInstance, { object: this });
+        },
+        /** @private */
+        _createGUID: function () {
+            this.GUID = this.parentClass.modelName + "_" + MBX.JsModel.nextGUID();
+        }
+    };
+    
+    /** 
+        @class the prototype of a Model
+        @constructor
+        @throws an error if there's no name, a name already exists or you specified a primaryKey and it wasn't a string
+    */
+    JsModel = function (name, opts) {
+        opts = opts || {};
+        if (!name) {
+            throw new Error("A name must be specified");
+        }
+        if (modelCache[name]) {
+            throw new Error("A model by that name exists");
+        }
+        if (opts.primaryKey && (typeof opts.primaryKey != "string")) {
+            throw new Error("primaryKey specified was not a string");
+        }
+        Object.extend(this, opts);
+        /** the model name of this model
+            @type String
+        */
+        this.modelName = name;
+        /** the instances of this model
+            @private
+        */
+        this.instanceCache = {};
+        
+        this.prototypeObject = MBX.Constructor(oneJsModelInstance);
+        /**
+            instances get their parentClass assigned this model
+            @name JsModel#instance.parentClass
+            @type JsModel
+        */
+        this.prototypeObject.parentClass = this;
+        
+        /** events that this model will fire. Use this to hook into (at a very low level) events
+            @example
+              MBX.EventHandler.subscribe(MBX.cssClass, MyModel.Event.newInstance, function (instance) { // dostuff } );
+        */
+        this.Event = {
+            newInstance: this.modelName + "_new_instance",
+            changeInstance: this.modelName + "_change_instance",
+            destroyInstance: this.modelName + "_destroy_instance"
+        };
+        
+        /** add an instanceMethods attribute to the passed in attributes in order to extend
+            all instances of this model.  You can also specify default attributes by adding
+            a defaults attribute to this attribute.
+            @type Object
+            @name JsModel.instanceMethods
+            @example
+              MyModel = MBX.JsModel.create("MyModel", {
+                  instanceMethods: {
+                      defaults: {
+                          myAttribute: "myDefault"
+                      },
+                      myMethod: function (method) {
+                          return this.get('myAttribute');
+                      }
+                  }
+              });
+              MyModel.create().myMethod() == "myDefault";
+        */
+        if (opts.instanceMethods) {
+            Object.extend(this.prototypeObject, opts.instanceMethods);
+        }
+        
+        modelCache[name] = this;
+        
+        MBX.EventHandler.fireCustom(document.body, "new_model", {
+            object: this
+        });
+    };
+    
+    JsModel.prototype = {
+        /**
+            Create an instance of the model
+            @param {Object} attrs attributes you want the new instance to have
+            @example
+              MyModel = MBX.JsModel.create("MyModel");
+              var instance = MyModel.create({
+                  myAttr: 'boo'
+              });
+              instance.get('myAttr') == 'boo';
+        */
+        create: function (attrs) {
+            attrs = attrs || {};
+            var obj = MBX.Constructor(this.prototypeObject);
+            
+            obj.attributes = {};
+            if (obj.defaults) {
+                Object.extend(obj.attributes, obj.defaults);
+            }
+            Object.extend(obj.attributes, attrs);
+            
+            if (this.validateObject(obj)) {
+                obj._createGUID();
+                this.cacheInstance(obj);
+                obj.errors = null;
+                MBX.EventHandler.fireCustom(document.body, this.Event.newInstance, {
+                    object: obj
+                });
+                if (typeof obj.afterCreate == "function") {
+                    obj.afterCreate();
+                }
+                return obj;
+            } else {
+                throw new Error("trying to create an instance with the same primary key as another instance")
+            }
+        },
+        
+        /** this method to get extended later.  Used mostly internally.  Right now it only verifies
+            that a primaryKey is allowed to be used
+            @param {JsModel#instance} instance the instance that's being validated
+        */
+        validateObject: function (instance) {
+            // temporarily - this only will validate primary keys
+            if (this.primaryKey) {
+                if (!instance.get(this.primaryKey)) {
+                    return false;
+                }
+                if (this.find(instance.get(this.primaryKey))) {
+                    return false;
+                }
+            }
+            
+            return true;
+        },
+        
+        /** use this to extend all instances of a single model
+            @param {Object} attrs methods and attributes that you want to extend all instances with
+        */
+        extendInstances: function (attrs) {
+            /** @default {} */
+            attrs = attrs || {};
+            Object.extend(this.JsModelInstance.prototype, attrs);
+        },
+        
+        /** store the instance into the cache
+            @private
+        */
+        cacheInstance: function (instance) {
+            if (this.primaryKey) {
+                this.instanceCache[instance.get(this.primaryKey)] = instance;
+            } else {
+                this.instanceCache[instance.GUID] = instance;
+            }
+        },
+        
+        /** find a single instance
+            @param {String} primaryKey a JsModel#instance primaryKey
+            @returns an instance of this element
+            @see JsModel#instance.primaryKey
+        */
+        find: function (primaryKey) {
+            return this.instanceCache[primaryKey];
+        },
+        
+        /** @returns all instances of this model */
+        findAll: function () {
+            return $H(this.instanceCache).values();
+        },
+        
+        // does this belong in the views?
+        /** given a domElement with certain classes - return the instance that it belongs to
+            @param {DomElement} el the element which has the correct classes on it
+            @returns an instance of this model or null
+        */
+        findByElement: function (el) {
+            el = $(el);        
+            var modelCss = MBX.JsTemplate.cssFromModel(this);
+            if (el.hasClassName(modelCss)) {
+                var match = el.className.match(new RegExp(modelCss + "_([^\\s$]+)"));
+                var returnInstance;
+                if (match) {
+                    $H(this.instanceCache).each(function (pair) {
+                        if (pair.key.gsub(/[^\w\-]/, "_").toLowerCase() == match[1]) {
+                            returnInstance = pair.value;
+                        }
+                    });
+                }
+                return returnInstance;
+            }
+        },
+        
+        /** return the number of instances that are in this model */
+        count: function () {
+            return this.findAll().length;
+        }
+    };
+    
+    publicObj.Event = {
+        newModel: "new_model"
+    };
+    
+    /**
+        Used for creating a new JsModel
+        @name MBX.JsModel.create
+        @function
+        
+        @param {String} name model name used to prevent name collision
+        @param {Object} opts defaults to {}
+        
+        @constructs
+        @example
+          var MyModel = MBX.JsModel.create("MyModel");
+          var instance = MyModel.create();
+    */
+    publicObj.create = function (name, opts) {
+        return new JsModel(name, opts);
+    };
+    
+    
+    /**
+       Used internally to find the next GUID
+       @private
+    */
+    publicObj.nextGUID = function () {
+        return currentGUID++;
+    };
+    
+    /**
+        Extends all JsModels
+        @name MBX.JsModel.extend
+        @function
+        @param {Object} methsAndAttrs the methods and attributes to extend all models with
+    */
+    publicObj.extend = function (methsAndAttrs) {
+        methsAndAttrs = methsAndAttrs || {};
+        Object.extend(JsModel.prototype, methsAndAttrs);
+    };
+    
+    /**
+        Extend all instances of all models
+        @name MBX.JsModel.extendInstancePrototype
+        @function
+        @param {Object} methsAndAttrs the methods and attributes to extend all models' instances with
+    */
+    publicObj.extendInstancePrototype = function (methsAndAttrs) {
+        Object.extend(oneJsModelInstance, methsAndAttrs);
+    };
+    
+    /**
+           Destroy a controller and unsubscribe its event listeners
+           @param {String} name the name of the controller
+           @name MBX.JsController.destroyModel
+           @function
+   */
+    publicObj.destroyModel = function (name) {
+        delete modelCache[name];
+    };
+    
+    return publicObj;
+})();
